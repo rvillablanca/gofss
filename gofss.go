07773e3de0566052e6e9ccefe60e7e5bffaa45dc
Rodrigo Villablanca <villa061004@gmail.com>
2020-04-17 13:58:30 -0400

    Apply gofmt

--- gofss.go
+++ gofss.go
@@ -1,67 +1,65 @@
 package gofss
 
 import (
-    "bytes"
-    "encoding/base64"
-    "encoding/json"
-    "fmt"
-    "io"
-    "net/http"
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 type Client interface {
-    GeneratePDF(string) ([]byte, error)
+	GeneratePDF(string) ([]byte, error)
 }
 
 type defaultClient struct {
-    serviceURL string
+	serviceURL string
 }
 
 func (c *defaultClient) GeneratePDF(html string) ([]byte, error) {
-    type msg struct {
-        Value string `json:"value"`
-    }
+	type msg struct {
+		Value string `json:"value"`
+	}
 
-    reqBuff := &bytes.Buffer{}
-    req := msg{Value: html}
-    err := json.NewEncoder(reqBuff).Encode(req)
-    if err != nil {
-        return nil, fmt.Errorf( "could not encode request: %w", err)
-    }
+	reqBuff := &bytes.Buffer{}
+	req := msg{Value: html}
+	err := json.NewEncoder(reqBuff).Encode(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not encode request: %w", err)
+	}
 
-    resp, err := http.Post(c.serviceURL, "application/json", reqBuff)
-    if err != nil {
-        return nil, fmt.Errorf("could not generate pdf: %w", err)
-    }
-    
-    defer closeQuietly(resp.Body)    
+	resp, err := http.Post(c.serviceURL, "application/json", reqBuff)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate pdf: %w", err)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        return nil, fmt.Errorf("invalid pdf service response code: %d", resp.StatusCode)
-    }
+	defer closeQuietly(resp.Body)
 
-    jsonResp := msg{}
-    err = json.NewDecoder(resp.Body).Decode(&jsonResp)
-    if err != nil {
-        return nil, fmt.Errorf("could not decode response from pdf service: %w", err)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid pdf service response code: %d", resp.StatusCode)
+	}
 
-    pdf, err := base64.StdEncoding.DecodeString(jsonResp.Value)
-    if err != nil {
-        return nil, fmt.Errorf("could not decode base64 pdf: %w", err)
-    }
+	jsonResp := msg{}
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode response from pdf service: %w", err)
+	}
 
-    return pdf, nil
+	pdf, err := base64.StdEncoding.DecodeString(jsonResp.Value)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode base64 pdf: %w", err)
+	}
+
+	return pdf, nil
 }
 
 func closeQuietly(c io.Closer) {
-    if c != nil {
-        _ = c.Close()
-    }
+	if c != nil {
+		_ = c.Close()
+	}
 }
 
 func New(serviceURL string) Client {
-    return &defaultClient{serviceURL: serviceURL}
+	return &defaultClient{serviceURL: serviceURL}
 }
-
-
